db5e6f5c16fb7b8775fbe6999b6eb5e210f77c85
rohitk5252 <rohitofficial2001@gmail.com>
2021-04-12 03:01:32 +0530

    new

--- quereka_project/settings.py
+++ quereka_project/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
